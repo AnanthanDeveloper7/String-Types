09ae97b564860ad8c44cd24d6e4b64dfd1e4b4f3
HARI <HARI@DESKTOP-TGPGN4M>
2022-07-30 18:26:54 +0530

    Commit

---
+++ Program2/src/Strbuf.java
@@ -0,0 +1,9 @@
+
+public class Strbuf {
+	public static void main (String[] args) {
+		StringBuffer s =new StringBuffer("java");
+		s.append("Prog");
+		System.out.println(s);
+	}
+
+}
---
+++ Program2/src/str.java
@@ -0,0 +1,9 @@
+
+public class str {
+	public static void main(String[] args) {
+		String s=new String("java");
+		s.concat("Prog");
+		System.out.println(s);
+	}
+
+}
